abcbacda90523e35daf28735b56e701e07ee7134
CS4752 <cs4752@en-cs-lgg03-07.coecis.cornell.edu>
2015-02-06 04:34:24 -0500

    cleaned everything up. TODO: bimanual controller

--- scripts/controller.py
+++ scripts/controller.py
@@ -8,170 +8,253 @@ import sys
 
 msg = State() #global State variable, defining symbolic world
 
-openGripper = 0
-closeGripper =1
-moveToBlock = 2
-moveOverBlock = 3
+#Define enumerators for move_robot actions
+openGripper 	= 0
+closeGripper 	= 1
+moveToBlock 	= 2
+moveOverBlock 	= 3
 
-def configure(configAction):
-	rospy.loginfo('Configuring action: %s',configAction)
+def set_config(config):
+	# Responsible for manipulation blocks into configurations, including scattering blocks, 
+	# putting blocks in ascending order after scattered, and putting blocks 
+	# in descending order after scattered.
+
+	# Set rate for sampling simulation state
     	rate = rospy.Rate(1) # 1hz
 	
-	#scatter blocks. Loop through all blocks to see if its under any
-	isvalid = 1
-	complete = 0
-	block = 1
-	while (isvalid and not complete):
+	# Set initial constants
+	isValid = 1	# indicates if manipulations are valid
+	complete = 0	# indicates if overall manipulation complete
+	block = 1	# initial block for manipulation
 
-		if (configAction == 'scatter'):	
+	# Continue manipulation loop while actions have been valid and sequencce is not complete
+	while (isValid and not complete):
+
+		if (config == 'scatter'):
+
+			# Scatter blocks. Find current block on top of stack by looping through all until find one	
 			block = 1		
 			while (block <= msg.num_blocks) and ((block in msg.block_under) or (msg.block_under[block-1] == 0)): 
 				block+=1
+
 			if (block > msg.num_blocks):
-				#all blocks checked. no more to move!
+				# All blocks checked; no more to move
 				complete=1
- 				#Do last check to make sure:
+
+ 				# Do last check to make sure
 				block = 1
 				while (block <= msg.num_blocks and msg.block_under[block-1] == 0):
 					block = block+1
 				if (block <= msg.num_blocks):
-					#there must have been an error :( 
-					isvalid = 0
+					# One block isn't over table (block 0). Must have been an uncaught manipulation error
+					isValid = 0
+			
 			else:
-				#block isn't under another block or on table, so move it!
-				rospy.loginfo("moving block %d",block)
+				# Block found on top of stack, so move it
+				rospy.loginfo("Moving block %d",block)
 				try:
+					# Proxy service
         				move_robot = rospy.ServiceProxy("move_robot", MoveRobot)
 					
-					#PRECONDITION: gripper already open
-					isvalid = move_robot(moveToBlock, block)
-					if (isvalid):
-						isvalid = move_robot(closeGripper,0)
-						if (isvalid):
-							isvalid = isvalid and move_robot(moveOverBlock,0)
-							if (isvalid):
-								isvalid = isvalid and move_robot(openGripper,0)
+					# Perform the following manipulation sequence:
+					# 	1) Move robot to block
+					#       2) Close gripper
+					#       3) Move robot over table (block 0)
+					#       4) Open gripper to drop block on table
+					#
+					# PRECONDITION: gripper already open. 
+					# Assumption: don't need to check state between each manipulation
+					# If one of the actions are invalid, quit imediately
+					isValid = move_robot(moveToBlock, block)
+					if (not isValid):
+						return 0
+					isValid = move_robot(closeGripper,0)
+					if (not isValid):
+						return 0
+					isValid = move_robot(moveOverBlock,0)
+					if (not isValid):
+						return 0
+					isValid = move_robot(openGripper,0)
+					if (not isValid):
+						return 0
+					# Sleep before next iteration					
+					rate.sleep()
+				
 
 				except rospy.ServiceException, e:
+					# Error with service. End manipulation process
         				rospy.loginfo("Service call failed: %s"%e)
-					isvalid = 0
-		elif (configAction == "scattered_to_ascending"):
-			rospy.loginfo("moving block %d",block)
+					isValid = 0
+
+		elif (config == "scattered_to_ascending"):
+			# Move blocks to ascending order, assuming blocks were previously scattered
+
+			rospy.loginfo("Moving block %d",block)
 			try:
+				# Proxy service
         			move_robot = rospy.ServiceProxy("move_robot", MoveRobot)
 					
-				#PRECONDITION: gripper already open
-				isvalid = move_robot(moveToBlock, block)
-				if (isvalid):
-					isvalid = move_robot(closeGripper,0)
-					if (isvalid):
-						isvalid = isvalid and move_robot(moveOverBlock,block-1)
-						if (isvalid):
-							isvalid = isvalid and move_robot(openGripper,0)
-				
+				# Perform the following manipulation sequence:
+				# 	1) Move robot to block
+				#       2) Close gripper
+				#       3) Move robot over previous (block i-1)
+				#       4) Open gripper to drop block onto other
+				#
+				# PRECONDITION: gripper already open. 
+				# If one of the actions are invalid, quit imediately
+				isValid = move_robot(moveToBlock, block)
+				if (not isValid):
+					return 0
+				isValid = move_robot(closeGripper,0)
+				if (not isValid):
+					return 0
+				isValid = move_robot(moveOverBlock,block-1)
+				if (not isValid):
+					return 0
+				isValid = move_robot(openGripper,0)
+				if (not isValid):
+					return 0
+
+				# Sleep before next iteration or completion check					
 				rate.sleep()
+
 				if (block == msg.num_blocks):
-					#did all blocks!
+					# All blocks manipulated
 					complete = 1
-					#check if valid
-					trueArr = ()
+	
+					# Check if manipulation was successful by comparing it to expected arangement
+					expected = ()
 					for i in range(0,msg.num_blocks):
-						trueArr+=(i,)
-					rospy.loginfo('%s',trueArr)
-					rospy.loginfo('%s',msg.block_under)
-					isvalid = trueArr == msg.block_under
-					if (not isvalid):
-						rospy.loginfo("complete, but not in ascending order!") 
+						expected+=(i,)
+					isValid = expected == msg.block_under
+					if (not isValid):
+						rospy.loginfo("Manipulation complete, but not in ascending order!") 
 				else:
+					# Manipulate next block
 					block+=1
-			except rospy.ServiceException, e:
-        			rospy.loginfo("Service call failed: %s"%e)
-				isvalid = 0
 
-		elif (configAction == "scattered_to_descending"):
-			block2 = msg.num_blocks-block+1
-			rospy.loginfo("moving block %d",block2)
+			except rospy.ServiceException, e:
+				# Error with service. End manipulation process
+        			rospy.loginfo("Service call failed: %s"%e)
+				isValid = 0
+
+		elif (config == "scattered_to_descending"):
+			# Move blocks to descending order, assuming blocks were previously scattered
+			blockToManipulate = msg.num_blocks-block+1	#manipulate in reverse order
+			rospy.loginfo("Moving block %d",blockToManipulate)
+
 			try:
+				# Proxy service
         			move_robot = rospy.ServiceProxy("move_robot", MoveRobot)
 					
-				#PRECONDITION: gripper already open
-				isvalid = move_robot(moveToBlock, block2)
-				if (isvalid):
-					isvalid = move_robot(closeGripper,0)
-					if (isvalid):
-						if block2 == msg.num_blocks:
-							overBlock = 0
-						else:
-							overBlock = block2+1
-						isvalid = isvalid and move_robot(moveOverBlock,overBlock)
-						if (isvalid):
-							isvalid = isvalid and move_robot(openGripper,0)
-				
+				#Perform the following manipulation sequence:
+				# 	1) Move robot to block
+				#       2) Close gripper
+				#       3) Move robot over proceeding block (block i+1)
+				#       4) Open gripper to drop block onto other
+				#
+				# PRECONDITION: gripper already open. 
+				# If one of the actions are invalid, quit imediately
+				isValid = move_robot(moveToBlock, blockToManipulate)
+				if (not isValid):
+					return 0
+				isValid = move_robot(closeGripper,0)
+				if (not isValid):
+					return 0
+				# Determine proceeding block
+				if blockToManipulate == msg.num_blocks:
+					overBlock = 0
+				else:
+					overBlock = blockToManipulate+1
+				isValid = move_robot(moveOverBlock,overBlock)
+				if (not isValid):
+					return 0
+				isValid = move_robot(openGripper,0)
+				if (not isValid):
+					return 0
+
+				# Sleep before next iteration or completion check					
 				rate.sleep()
 				if (block == msg.num_blocks):
-					#did all blocks!
+					# Did all blocks!
 					complete = 1
-					#check if valid
-					trueArr = ()
-					for i in range(2,msg.num_blocks+1):
-						trueArr+=(i,)
-					trueArr+=(0,)
-					rospy.loginfo('%s',trueArr)
-					rospy.loginfo('%s',msg.block_under)
-					isvalid = trueArr == msg.block_under
-					if (not isvalid):
-						rospy.loginfo("complete, but not in descending order!") 
-				else:
-					block+=1
-			except rospy.ServiceException, e:
-        			rospy.loginfo("Service call failed: %s"%e)
-				isvalid = 0
-		if (isvalid):
-			#movement successful. Wait to get new robot state
-			if (not complete):
-				rate.sleep()
-		else:
-			#error somewhere
-			rospy.loginfo("Failed to move on block %d",block)
-	if (complete):
-		rospy.loginfo("Configuration set successful!")
 
-	return isvalid
+					# Check if manipulation was successful by comparing it to expected arangement
+					expected = ()
+					for i in range(2,msg.num_blocks+1):
+						expected+=(i,)
+					expected+=(0,)
+					isValid = expected == msg.block_under
+					if (not isValid):
+						rospy.loginfo("Manipulation complete, but not in ascending order!") 
+				else:
+					# Manipulate next block
+					block+=1
+
+			except rospy.ServiceException, e:
+				# Error with service. End manipulation process        			
+				rospy.loginfo("Service call failed: %s"%e)
+				isValid = 0
+
+	return isValid
 
 def callback_state(data):
 	global msg
 	msg = data
 
 def callback_command(data):
+	# Callback for "command" topic subscriber. Takes in data (String type), and responds to the command. 
+	# The three possible commands are: 
+	# 
+	# "scattered":		all blocks on table
+	# "ascend_stacked":	blocks aranged in increasing order from ground up
+	# "descend_stacked":	blocks aranged in decreasing order from ground up
 
+	# Get command
 	command = data.data
 	rospy.loginfo(command)
 	
+	# Respond to command
 	if (command == 'scattered'):
-		isvalid = configure('scatter')
+		# Scatter blocks
+		isvalid = set_config('scatter') 
 	elif (command == 'ascend_stacked'):
-		isvalid = configure('scatter')
-		isvalid = isvalid and configure('scattered_to_ascending')
+		# Scatter blocks, then put in ascending order
+		isvalid = set_config('scatter')
+		isvalid = isvalid and set_config('scattered_to_ascending')
 	elif (command == 'descend_stacked'):
-		isvalid = configure('scatter')
-		isvalid = isvalid and configure('scattered_to_descending')
+		# Scatter blocks, then put in descending order
+		isvalid = set_config('scatter')
+		isvalid = isvalid and set_config('scattered_to_descending')	
+	else:
+		# Unknown command
+		isvalid = 0;
+		rospy.loginfo("Unkown command sent to controller.")
 
-	
-
-	
-	
+	# Check if manipulation occured successfully
+	if (isvalid):
+		rospy.loginfo("Commands execution successful")
+	else:
+		rospy.loginfo("Command execution incomplete")
 
 def controller():
-	
+	# Controller for simple block manipulation simulator. Subscribes to topics "state", which 
+	# indicates state of the simulator's symbolic world, and "command", which listens for configuration commands
+	# published for the controller
+
+	# Wait until the move_robot service is established
 	rospy.wait_for_service('move_robot')
 
+	# Initialize node
 	rospy.init_node('controller', anonymous=True)
 
-	rospy.Subscriber("command", String, callback_command)
+	# Subscribe to "state" and "command" topics
 	rospy.Subscriber("state", State, callback_state)
+	rospy.Subscriber("command", String, callback_command)
 
-	# spin() simply keeps python from exiting until this node is stopped
+	# Keep python from exiting until this node is stopped
 	rospy.spin()
 
 if __name__ == "__main__":
-	controller()
+	# Run controller
+	controller() 
--- scripts/sim_master.py
+++ scripts/sim_master.py
@@ -5,12 +5,25 @@ from sjd227_hw1.msg import *
 from std_msgs.msg import String
 import rospy
 
-msg = State() #global State variable, defining symbolic world
+msg = State() #global State variable, defining symbolic world of simulation
 
 def handle_move_robot(req):
-	isvalid = 1
-	print "Moving Robot for action: %s, target: %s"%(req.Action, req.Target)
+	# Handles the service "MoveRobot.srv", for which the request has an Action and Target field 
+	# and response has isValid field:
+	#	Action:
+	#		0 --> open gripper
+	#		1 --> close gripper
+	#		2 --> move to block (i.e., gripper around block)
+	#		3 --> move over block (i.e., gripper above block)
+	#	Target:
+	#		n --> nth block, where block 0 represents the table
+	#	---
+	#	isValid: boolean indicating whether action can be performed
 	
+	#Initialize action as valid by default
+	isValid = 1 
+
+	#Determine action, and act appropriately
 	if (req.Action == 0):
 		#open gripper
 		if (msg.gripper_state == 1):
@@ -24,23 +37,17 @@ def handle_move_robot(req):
 				#set no block in gripper
 				msg.block_in_gripper = 0
 				
-		else:
-			#gripper already open, so it's an invalid action
-			isvalid = 0
 	elif (req.Action == 1):
 		#close gripper
 		if (msg.gripper_state == 0):
 			#gripper is currently open, so close it
 			msg.gripper_state = 1
-		else:
-			#gripper is already closed, so it's an invalid action
-			isvalid = 0
 	
 	elif (req.Target <= msg.num_blocks):
 
-		if (req.Action == 2 and req.Target > 0):
+		if (req.Action == 2):
 			#move to block
-			if ((msg.gripper_state == 0) and (req.Target == 0 or req.Target not in msg.block_under)):
+			if ((msg.gripper_state == 0) and (req.Target > 0) and (req.Target not in msg.block_under)):
 				#gripper is open, and target isn't under any block, so 
 				#ok to move to block.
 				#record new block in gripper and one underneath
@@ -49,77 +56,88 @@ def handle_move_robot(req):
 				msg.block_under_gripper = msg.block_under[i-1] 
 			else:
 				#gripper is closed, so can't move to block
-				isvalid = 0
+				isValid = 0
 		elif (req.Action == 3):
 			#move over block
-		
 			if (req.Target ==0 or req.Target not in msg.block_under):
 				#no blocks over target, so move over it
 				msg.block_under_gripper = req.Target
-			
 				if (msg.gripper_state == 0):
 					#gripper was open, so block previously in gripper region
 					#didn't travel along with the gripper
 					msg.block_in_gripper = 0
-	
 			else:
-				#a block is already over target, so this action is invalid
-				isvalid = 0
+				#some block is already over target, so this action is invalid
+				isValid = 0
 		else:
 			#unknown action	
-			isvalid = 0
+			isValid = 0
 	else:
 		#unknown target
-		isvalid = 0
-	return MoveRobotResponse(isvalid)
+		isValid = 0
+	
+	#Send out response
+	return MoveRobotResponse(isValid)
 	   
 def sim_master_server():
+	# Master server for this simple simulation. Handles service "move_robot" for 
+	# commanding the robot to perform an action on a specific block, and 
+	# publishes to the topic "state" the current state of the world it sees, "State", 
+	# with features described as follows:
+	#
+	# gripper_state:	0 (open) or 1 (closed)
+	# block_in_gripper: 	0 (no block) or n (nth block) within gripper. Gripper could be open or closed around block
+	# block_under_gripper:	0 (no block) or n (nth block) under gripper
+	# num_blocks:		n (n blocks in simulation)
+	# block_under:		1xn array, where ith element corresponds to block underneath ith block. Table is block 0. 
 
-	print "HI"
 	#Initialize node
 	rospy.init_node('sim_master_server')
-	s = rospy.Service('move_robot', MoveRobot, handle_move_robot)
-	print "Ready to move robot"
 
-	#get params
+	#Create Service "move_robot"
+	s = rospy.Service('move_robot', MoveRobot, handle_move_robot)
+
+	#Get global parameters
 	n = rospy.get_param("/num_blocks")
 	config = rospy.get_param("/configuration")
 
-	#initialize the gripper as being open, with no block, over table
+	#Initialize the gripper as being open, with no block, over table
 	gripper_state = 0
 	block_in_gripper = 0
 	block_under_gripper = 0
 
-	#determine initial state of world
+	#Determine initial state of world
 	block_under = []
-	str1= "test"
+
 	if (config == "scattered"):
+		#scattered config. --> all blocks are on table
 		for i in range(0,n):
 			block_under.append(0)
-			#block_under_i += (0,)
+
 	elif (config == "stacked_descending"):
+		#stacked, descending config. --> blocks descend in number from ground up
 		for i in range(2,n+1):
 			block_under.append(i)
-			#block_under_i += (i,)
 		block_under.append(0)
-		#block_under_i+= (0,)
+
 	elif (config == "stacked_ascending"):
+		#stacked, ascending config. --> blocks ascend in number from ground up
 		for i in range(0,n):	
 			block_under.append(i)	
-			#block_under_i+= (i,)
+
 	else:
-		print "Invalid Configuration!"
+		rospy.loginfo("Invalid initial configuration.")
 		return
 
-	#create message
+	#Assemble message
 	global msg
 	msg = State(gripper_state,block_in_gripper,block_under_gripper,n,block_under);
 
-	#initialize publishing    	
+	#Initialize topic "state" to publish to    	
 	pub = rospy.Publisher('state', State, queue_size=10)
-    	rate = rospy.Rate(1) # 1hz
+    	rate = rospy.Rate(1) # 1hz publish rate
 
-	#publish state of the world    
+	#Publish state of the world    
 	while not rospy.is_shutdown():
 		#rospy.loginfo(msg)
 		pub.publish(msg)
@@ -129,7 +147,7 @@ def sim_master_server():
  
 if __name__ == "__main__":
 	try:
-		print "main"
+		#Run simulation master server
 		sim_master_server()
 	except rospy.ROSInterruptException:
         	pass
